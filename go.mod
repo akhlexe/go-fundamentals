00be579c224a990acd02e05106df1c3b32b27837
Exequiel Piñero <Exequiel Piero@DESKTOP-ONUD604>
2023-12-06 18:29:44 -0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akhlexe/go-fundamentals
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/akhlexe/go-fundamentals/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
